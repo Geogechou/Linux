526db90063e334b4a433ab7bad27ec85cb8c567e
george <usasne@163.com>
2020-03-03 19:41:30 +0800

    get

--- c/GET请求/list.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by george on 19/8/4.
-//
-#include<stdio.h>
-void print()
-{
-    printf("hello from list.c");
-}
--- c/GET请求/list.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by george on 19/8/4.
-//
-
-#ifndef UNTITLED_LIST_H
-#define UNTITLED_LIST_H
-void print();
-#endif //UNTITLED_LIST_H
